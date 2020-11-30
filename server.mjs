bccf94cc0f87d8811ad7a11301c797e03da04821
mchaikov <m.chaikov@artsofte.ru>
2020-11-30 21:52:58 +0500

    6

--- server.mjs
+++ server.mjs
@@ -10,18 +10,60 @@ const rootDir = process.cwd();
 const port = 3000;
 const app = express();
 
-app.get("/client.mjs", (_, res) => {
-  res.header("Cache-Control", "private, no-cache, no-store, must-revalidate");
-  res.sendFile(path.join(rootDir, "client.mjs"), {
-    maxAge: -1,
-    cacheControl: false,
-  });
+// вот тут не работает
+const checkSession = (req, res, next) => {
+    console.log("КУКИКУКИКУКИКУКИКУКИКУКИКУКИКУКИ", req.cookies, "КУКИКУКИКУКИКУКИКУКИКУКИКУКИКУКИКУКИ")
+        // if (req.cookies === undefined && req.url == "/login" || !req.url.includes("api") || !req.url.includes("static")) {
+        // next()
+        // } else
+        // res.redirect("/login")
+    console.log(req.url)
+    if (req.cookies === undefined && !req.url.includes('login')) {
+        console.log('redirecting')
+        res.redirect('https://metanit.com/')
+    } else {
+        next()
+    }
+}
+
+app.get("/client.mjs", checkSession, (_, res) => {
+    res.header("Cache-Control", "private, no-cache, no-store, must-revalidate");
+    res.sendFile(path.join(rootDir, "client.mjs"), {
+        maxAge: -1,
+        cacheControl: false,
+    });
 });
 
-app.get("/", (_, res) => {
-  res.send(":)");
+app.use(cookieParser())
+app.use(bodyParser.json())
+app.use(express.static('spa/build'))
+
+let userName = ""
+
+app.get("/api/getUser", (req, res) => {
+    res.send({ "userName": req.cookies.userName })
+})
+
+app.post("/api/loginUser", (req, res) => {
+    userName = req.body["userName"];
+    res.cookie("userName", userName, { httpOnly: true, secure: true, sameSite: true })
+    res.send({ "userName": userName })
+})
+
+app.post("/api/logoutUser", (req, res) => {
+    userName = '';
+    res.cookie("userName", userName, { httpOnly: true, secure: true, sameSite: true })
+    res.send({ "userName": userName })
+})
+
+app.get("*", checkSession, (_, res) => {
+    res.sendFile(path.join(rootDir, "/spa/build/index.html"))
 });
 
-app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
+https.createServer({
+        key: fs.readFileSync('certs/server.key'),
+        cert: fs.readFileSync('certs/server.cert')
+    },
+    app).listen(port, () => {
+    console.log(`App listening on port ${port}`);
 });
