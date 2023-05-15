e103011827587ae6fe63cff70fdde3f1513cf97c
JustASpace <js4std@gmail.com>
2023-05-15 15:12:57 +0500

    1-7

--- server.mjs
+++ server.mjs
@@ -4,7 +4,6 @@ import express from "express";
 import https from "https";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import fetch from "node-fetch";
 
 const rootDir = process.cwd();
 const port = 3000;
@@ -18,10 +17,57 @@ app.get("/client.mjs", (_, res) => {
   });
 });
 
+
+app.use(bodyParser.json());
+app.use(cookieParser());
+
 app.get("/", (_, res) => {
   res.send(":)");
 });
 
-app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
+app.get('/api/getUser', (req, res) => {
+  res.json({
+    user: req.cookies.user
+  })
 });
+
+app.post('/api/loginUser', (req, res) => {
+  const user = req.body.user;
+  res.cookie('user', user, {
+    httpOnly: false,
+    secure: true,
+    sameSite: 'strict'
+  });
+  console.log(req.body);
+  console.log(user);
+  res.json({
+    user: user
+  });
+})
+
+app.post('/api/logoutUser', (req, res) => {
+  res.clearCookie('user');
+  res.send();
+})
+
+app.get('*', (req, res, next) => {
+  if (req.cookies.user === undefined) {
+    res.redirect('/login');
+  } else {
+    next();
+  }
+})
+
+app.use(express.static('spa/build'));
+
+app.get("*", function(req, res) {
+  res.sendFile(path.join(rootDir, "spa", "build", "index.html"));
+})
+
+https.createServer({
+  key: fs.readFileSync('certs/server.key'),
+  cert: fs.readFileSync('certs/server.cert')
+}, app)
+    .listen(port, () => {
+      console.log(`App listening on port ${port}`);
+    });
