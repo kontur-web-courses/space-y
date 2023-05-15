30149fa8fb1d1b87dc3d2a02507eb91eea1bf557
Timur Rakhimianov <raktagir2@gmail.com>
2023-05-15 15:59:47 +0500

    qwerty

--- server.mjs
+++ server.mjs
@@ -3,12 +3,34 @@ import fs from "fs";
 import express from "express";
 import https from "https";
 import cookieParser from "cookie-parser";
-import bodyParser from "body-parser";
-import fetch from "node-fetch";
 
 const rootDir = process.cwd();
 const port = 3000;
 const app = express();
+
+const loginedUsers = {};
+
+app.use(express.static('spa/build'));
+app.use(cookieParser());
+app.use(express.json());
+
+const isAuthorized = function (req, res, next) {
+  if (req.originalUrl.includes('api') ||
+      req.originalUrl.includes('static') ||
+      req.originalUrl.includes('login')) {
+    next();
+    return;
+  }
+  if ("username" in req.cookies) {
+    next();
+    return;
+  }
+  res.redirect("/login");
+  next();
+}
+
+app.use(isAuthorized);
+app.use(isAuthorized);
 
 app.get("/client.mjs", (_, res) => {
   res.header("Cache-Control", "private, no-cache, no-store, must-revalidate");
@@ -22,6 +44,33 @@ app.get("/", (_, res) => {
   res.send(":)");
 });
 
-app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
+app.get("*", (_, res) => {
+  res.sendFile(path.join(rootDir, "spa/build/index.html"));
+})
+
+app.post("/api/login", (req, res) => {
+  let username = req.body.username;
+  loginedUsers[username] = true;
+  res.cookie('username', username, {secure: true, httpOnly: true, sameSite: true});
+  res.json({username: username});
 });
+
+
+app.post("/api/unlogin", (req, res) => {
+  let username = req.body.username;
+  delete loginedUsers[username];
+  res.json({status: true});
+});
+
+https.createServer(
+        {
+          key: fs.readFileSync("certs/server.key"),
+          cert: fs.readFileSync("certs/server.cert"),
+        },
+        app
+    )
+    .listen(3000, function () {
+      console.log(
+          "Example app listening on port 3000! Go to https://localhost:3000/"
+      );
+    });
